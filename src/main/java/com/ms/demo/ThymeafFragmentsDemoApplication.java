c8089966736d0d30b6d523e0f643816322b2e2bd
Mo <127251605+mosenyonjo@users.noreply.github.com>
2023-04-09 07:54:40 +0100

    Initial project commit
    Initial project commit for a a springboot demo project using Thymeleaf fragments and version-agnostic webjars

---
+++ src/main/java/com/ms/demo/ThymeafFragmentsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeafFragmentsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeafFragmentsDemoApplication.class, args);
+	}
+
+}
